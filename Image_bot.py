c9150daeb654e9854c105d1c4ab024abc7822f3e
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-27 22:18:47 +0600

    Создание бота, обработка комманд

---
+++ Image_bot.py
@@ -0,0 +1,31 @@
+import telebot
+import webbrowser
+
+bot = telebot.TeleBot('6959262170:AAFZYR7aJG5sI2j2NfD44F9VNqc5gzsoH4Q')
+
+
+
+
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    webbrowser.open('https://github.com/Maxxx-VS?tab=repositorie')
+
+
+@bot.message_handler(commands=['start', 'main', 'hello'])
+def main(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, '<b>Help</b> <em><u>inform</u></em>', parse_mode='html')
+
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+
+bot.polling(non_stop=True)
