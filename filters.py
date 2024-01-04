9e264e1ff8f72f120561b744c2369018ab046da7
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-01-04 19:18:16 +0600

    Бот по обработке фотографий - возвращает фото в чат - неправильно сохраняет фото

--- filters.py
+++ filters.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-img = Image.open("C:/Users/hot-z/pythonProject_Bot/photos/file_0.jpeg").convert('RGB')
+img = Image.open("C:/Users/hot-z/pythonProject_Bot/photos/file_0.jpg").convert('RGB')
 width, height = img.size
 
 def DarkFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
@@ -33,11 +33,11 @@ def BlueFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
         result = [int(r*1), int(g*1), int(b*3)]
     return tuple(result)
 
-def apply_filter(img: Image.Image, DarkFilter) -> Image.Image:
+def apply_filter(img: Image.Image, filt) -> Image.Image:
     for i in range(img.width):
         for j in range(img.height):
             r,g,b = img.getpixel((i, j))
-            new_pixel = DarkFilter(r,g,b)
+            new_pixel = filt(r,g,b)
             img.putpixel((i, j), new_pixel)
     img.save("C:/Users/hot-z/pythonProject_Bot/photos/file_0_NEW.jpeg")
     return img
