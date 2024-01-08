8d47330438db5b25e8c16dcc9eb4ca8e07fbaecf
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-01-08 22:09:50 +0600

    Бот по обработке фотографий. Не решен вопрос с пролной очисткой истории

--- filters.py
+++ filters.py
@@ -1,15 +1,15 @@
 import webbrowser
 import telebot
+import os
 from telebot import types
 from PIL import Image
 bot = telebot.TeleBot('6959262170:AAFZYR7aJG5sI2j2NfD44F9VNqc5gzsoH4Q')
-
-img = Image.open("C:/Users/hot-z/pythonProject_Bot/photos/file_0.jpg").convert('RGB')
-width, height = img.size
-
 @bot.message_handler(commands=['help', 'info'])
 def start(message):
     bot.send_message(message.chat.id, f'<em>Витрина на оформлении 😁</em>', parse_mode='html')
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    webbrowser.open('https://github.com/Maxxx-VS?tab=repositorie')
 @bot.message_handler(commands=['start', 'main', 'hello'])
 def start(message):
     bot.send_message(message.chat.id, f'Привет 👋🏻, {message.from_user.first_name}!')
@@ -21,13 +21,10 @@ def get_photo(message):
     try:
         file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id) # загружаем фотографию
         downloaded_file = bot.download_file(file_info.file_path)
-        # src = 'C:/Users/hot-z/pythonProject_Bot/photos/' + message.document.file_12345
+        #downloaded_file = bot.download_file('C:/Users/hot-z/pythonProject_Bot/photos/file_123.jpg')
         src = './photos/' + file_info.file_path.replace('photos/', '')
-        print(src)
-        print(file_info.file_path)
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
-            #new_file.close()
     except Exception as e:
         bot.reply_to(message, e)
     markup = types.InlineKeyboardMarkup()
@@ -60,41 +57,39 @@ def callback_message(callback):
         img = apply_filter(img, filt)
         img.show()
     processing()
-    bot.send_photo(callback.from_user.id, img)
+    bot.send_photo(callback.from_user.id, img) # возвращает фото с фильтром в чат
     bot.send_message(callback.message.chat.id, f"<b>ЗАГРУЖАЙ ЕЩЁ:</b>", parse_mode='html')
-@bot.message_handler(commands=['site', 'website'])
-def site(message):
-    webbrowser.open('https://github.com/Maxxx-VS?tab=repositorie')
 
 def DarkFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
         result = [int(r/3), int(g/3), int(b/3)]
     return tuple(result)
-
 def BrightFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
         result = [int(r*3), int(g*3), int(b*3)]
     return tuple(result)
-
 def RedFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
         result = [int(r*3), int(g*1), int(b*1)]
     return tuple(result)
-
 def GreenFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
         result = [int(r*1), int(g*3), int(b*1)]
     return tuple(result)
-
 def BlueFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
         result = [int(r*1), int(g*1), int(b*3)]
     return tuple(result)
+
+img = Image.open("C:/Users/hot-z/pythonProject_Bot/get.jpeg").convert('RGB')
+width, height = img.size
+print(img.width)
+print(img.height)
 
 def apply_filter(img: Image.Image, filt) -> Image.Image:
     for i in range(img.width):
@@ -104,14 +99,6 @@ def apply_filter(img: Image.Image, filt) -> Image.Image:
             img.putpixel((i, j), new_pixel)
     img.save("C:/Users/hot-z/pythonProject_Bot/photos/processing_photo_on.jpeg")
     return img
-
-
-
-
-
-
-
-
 
 bot.polling(non_stop=True)
 
--- ppb.py
+++ ppb.py
@@ -1,10 +1,11 @@
 import webbrowser
 import telebot
+import os
 from telebot import types
 from PIL import Image
 bot = telebot.TeleBot('6959262170:AAFZYR7aJG5sI2j2NfD44F9VNqc5gzsoH4Q')
 @bot.message_handler(commands=['help', 'info'])
-def start(message):
+def info(message):
     bot.send_message(message.chat.id, f'<em>Витрина на оформлении 😁</em>', parse_mode='html')
 @bot.message_handler(commands=['site', 'website'])
 def site(message):
@@ -16,11 +17,19 @@ def start(message):
                                       f'или сделай селфи:')
 @bot.message_handler(content_types=['photo', 'document'])
 def get_photo(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn_00 = types.KeyboardButton('👆 УАЛИТЬ ВЕСЬ КОНТЕНТ 👆')
+    markup.add(btn_00)
+    audio = open(r'Voice.mp3', 'rb')
+    bot.send_chat_action(message.from_user.id, 'upload_audio')
+    bot.send_audio(message.from_user.id, audio, reply_markup=markup)
+    audio.close()
+    bot.register_next_step_handler(message, on_click)
+    #bot.send_message(message.chat.id, 'Изображение получено', reply_markup=markup)
     global src
     try:
         file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id) # загружаем фотографию
         downloaded_file = bot.download_file(file_info.file_path)
-        #downloaded_file = bot.download_file('C:/Users/hot-z/pythonProject_Bot/photos/file_123.jpg')
         src = './photos/' + file_info.file_path.replace('photos/', '')
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
@@ -32,12 +41,20 @@ def get_photo(message):
     btn_3 = types.InlineKeyboardButton('Красный фильтр', callback_data='red_filter')
     btn_4 = types.InlineKeyboardButton('Зеленый фильтр', callback_data='green_filter')
     btn_5 = types.InlineKeyboardButton('Синий фильтр', callback_data='blue_filter')
-    btn_6 = types.InlineKeyboardButton("Developer's Git", url='https://github.com/Maxxx-VS?tab=repositorie')
+    # btn_6 = types.InlineKeyboardButton('Удалить последее фото', callback_data='delete')
+    btn_7 = types.InlineKeyboardButton("Developer's Git", url='https://github.com/Maxxx-VS?tab=repositorie')
     markup.row(btn_1, btn_2)
     markup.row(btn_3, btn_4, btn_5)
-    markup.row(btn_6)
+    markup.row(btn_7)
     bot.reply_to(message, f"<b>ВЫБЕРИ ОДИН ФИЛЬТР:\n"
                           f"и немного подожди...</b>", reply_markup=markup, parse_mode='html')
+
+def on_click(message):
+    if message.text == '👆 УАЛИТЬ ВЕСЬ КОНТЕНТ 👆':
+        for i in range (0, 100):
+            bot.delete_message(message.chat.id, message.id - i)
+        bot.send_message(message.chat.id, "Весь контент НЕ удалён!\n"
+                                          "пока не работает...")
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     if callback.data == 'dark_filter':
@@ -48,9 +65,12 @@ def callback_message(callback):
         filt = RedFilter
     elif callback.data == 'green_filter':
         filt = GreenFilter
-    else:
+    elif callback.data == 'blue_filter':
         filt = BlueFilter
     def processing():
+        # markup = types.ReplyKeyboardMarkup()
+        # btn = types.ReplyKeyboardMarkup("УДАЛИТЬ ПОСЛЕДНЕЕ ФОТО")
+        # markup.add(btn)
         global img
         img = Image.open(src).convert('RGB')
         img = apply_filter(img, filt)
@@ -62,33 +82,31 @@ def callback_message(callback):
 def DarkFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
-        result = [int(r/3), int(g/3), int(b/3)]
+        result = [int(r/4), int(g/4), int(b/4)]
     return tuple(result)
 def BrightFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
-        result = [int(r*3), int(g*3), int(b*3)]
+        result = [int(r*4), int(g*4), int(b*4)]
     return tuple(result)
 def RedFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
-        result = [int(r*3), int(g*1), int(b*1)]
+        result = [int(r*4), int(g*1), int(b*1)]
     return tuple(result)
 def GreenFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
-        result = [int(r*1), int(g*3), int(b*1)]
+        result = [int(r*1), int(g*4), int(b*1)]
     return tuple(result)
 def BlueFilter(r: int, g: int, b: int) -> tuple[int, int, int]:
     result = []
     for color in (r, g, b):
-        result = [int(r*1), int(g*1), int(b*3)]
+        result = [int(r*1), int(g*1), int(b*4)]
     return tuple(result)
 
 img = Image.open("C:/Users/hot-z/pythonProject_Bot/get.jpeg").convert('RGB')
 width, height = img.size
-print(img.width)
-print(img.height)
 
 def apply_filter(img: Image.Image, filt) -> Image.Image:
     for i in range(img.width):
@@ -99,7 +117,13 @@ def apply_filter(img: Image.Image, filt) -> Image.Image:
     img.save("C:/Users/hot-z/pythonProject_Bot/photos/processing_photo_on.jpeg")
     return img
 
+
+
+
 bot.polling(non_stop=True)
+
+
+
 
 
 
